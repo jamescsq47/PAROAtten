293bfb2f17719162e6dafc49b325c44b5f8c5356
hkeee21 <hjkl1379991@126.com>
2025-03-08 13:49:15 +0800

    [PARO] clean up

--- bench/test.py
+++ bench/test.py
@@ -1,7 +1,7 @@
 import torch
 from flash_attn.utils.benchmark import benchmark_forward
 
-import sageattention._qattn_sm80 as qattn
+import paroattention._qattn_sm80 as qattn
 
 import argparse
 
@@ -18,7 +18,7 @@ def process_tensor(tensor):
     return result
 
 
-parser = argparse.ArgumentParser(description='Benchmark QK Int8 PV FP16')
+parser = argparse.ArgumentParser(description='Benchmark QK INT8 PV INT8')
 parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
 parser.add_argument('--num_heads', type=int, default=32, help='Number of heads')
 parser.add_argument('--head_dim', type=int, default=128, help='Head dimension')
@@ -30,7 +30,7 @@ head = args.num_heads
 batch = args.batch_size
 headdim = args.head_dim
 
-print(f"CUDA QK Int4 PV FP16 Benchmark")
+print(f"PAROAttention QK Int8 PV INT8 Benchmark with Varying Sparsity.")
 print(f"batch: {batch}, head: {head}, headdim: {headdim}, pv_accum_dtype: {args.pv_accum_dtype}")
 
 WARP_Q = 16 if (headdim == 128 and args.pv_accum_dtype == "fp16+fp32") else 32
@@ -52,11 +52,9 @@ elif args.pv_accum_dtype == 'fp16':
 
 _qk_quant_gran = 3 if args.quant_gran == 'per_thread' else 2
 
-for sparse_ratio in {1.0}:
-    # print(f"sparse_ratio: {sparse_ratio}")
+for sparse_ratio in {0.2, 0.3, 0.5}:
     is_causal = False
     _is_causal = 1 if is_causal else 0
-    # print(f"is_causal: {is_causal}")
     for seq_len in {1024, 2048, 4096, 8192, 16384, 32768}: #1024, 2048, 4096, 8192, 16384, 32768
         sparse = torch.zeros((batch*head*seq_len*seq_len//4096), dtype=bool).cuda()
         random_tensor = torch.rand(sparse.shape).cuda()
@@ -76,19 +74,13 @@ for sparse_ratio in {1.0}:
             q_scale = torch.randn(batch, head, seq_len // WARP_Q * 8, dtype=torch.float).cuda()
             k_scale = torch.randn(batch, head, seq_len // WARP_K * 4, dtype=torch.float).cuda()
         
-        # v = torch.randn(batch, seq_len, head, headdim, dtype=torch.float16).cuda()
         v = torch.randint(-7, 8,(batch, seq_len, head, headdim), dtype=torch.int8).cuda()
         o_int4 = torch.empty(batch, seq_len, head, headdim, dtype=torch.float16).cuda()
         o_int8 = torch.empty(batch, seq_len, head, headdim, dtype=torch.float16).cuda()
         sm_scale = 1 / (headdim ** 0.5)
-        # for i in range(5): kernel_int8(q, k, v, o_int8, q_scale, k_scale, 0, _is_causal, _qk_quant_gran, sm_scale, 0,1000)
-        # torch.cuda.synchronize()
-        # _, time_int8 = benchmark_forward(kernel_int8, q, k, v, o_int8, q_scale, k_scale, 0, _is_causal, _qk_quant_gran, sm_scale, 0,1000, repeats=100, verbose=False, desc='Triton')
         for i in range(5): kernel_int8(q_compact, k_compact, v, o_int4, q_scale, k_scale, 0, _is_causal, _qk_quant_gran, sm_scale, 0,sparse)
         torch.cuda.synchronize()
         _, time_int8 = benchmark_forward(kernel_int8, q_compact, k_compact, v, o_int4, q_scale, k_scale, 0, _is_causal, _qk_quant_gran, sm_scale, 0, sparse,repeats=100, verbose=False, desc='Triton')
-        # print(o_int4)
-        # print(o_int8)
-        print(f'sparse ratio: {sparse_ratio}, latency:{time_int8.mean*1e3}')
-        # print(f'latency:{time_int4.mean}')
-        # print(f'int4 {seq_len} flops:{flops/time_int8.mean*1e-12}')
+    
+        print(f'seq len: {seq_len}, sparse ratio: {sparse_ratio}, latency:{time_int8.mean*1e3}')
+       
